a433734dedb76e2eca8fd9f539be4d5a8aa1ea56
Deivisma97 <david.i.herrera97@gmail.com>
2022-04-13 22:15:36 -0300

    javascript prueba

---
+++ script.js
@@ -0,0 +1,34 @@
+/* alert("Hola mundo") */
+
+
+let alumno = "deival"
+let nota = 9
+
+const entidad = "académica"
+
+
+
+
+
+
+console.log ("el nombre mio es " + alumno + " y su nota es " + nota)
+
+let str1 = "Lucas"
+let str2 = "Agustin"
+let str3 = "Roberto"
+
+let str4 = str1 + " y " + str2
+let str5 = `${str1} y ${str2}`
+
+console.log (str4)
+console.log (str5)
+
+let num1 = parseFloat(prompt("mete el 1er numero"))
+let num2 = parseFloat(prompt("mete el 2do numero"))
+
+let suma = num1 + num2
+let mult = num1 * num2
+
+console.log ("la suma es:" + suma)
+console.log ("la multiplicacion es:" + mult)
+
